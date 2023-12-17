de62233e45e609e6722d7a7c0f32e593d7c4d575
s5g4 <uucona@gmail.com>
2023-12-17 16:32:46 +0900

    formにjson表示

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useCallback, useState } from 'react';
+
+import validator from '@rjsf/validator-ajv8';
+import { RJSFSchema } from '@rjsf/utils';
+import Form from '@rjsf/core';
+//import Form from '@rjsf/material-ui';
+
+const schema: RJSFSchema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+};
+
+const log = (type) => console.log.bind(console, type);
+
+/*
+const formData = {
+  title: 'First taskeee',
+  done: true,
+};
+*/
+
 
 function App() {
+
+  const [value, setValue] = useState({});
+  const onChange = useCallback((e) => {
+    setValue(e.formData);
+  }, []);
+  const onSubmit = useCallback(() => {
+    console.log(value);
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Form
+        schema={schema}
+        validator={validator}
+        formData={value}
+        onChange={onChange} // 追加
+        onSubmit={onSubmit} // 追加
+        onError={log('errors')}
+      />
+      <hr />
+
+
+      <textarea cols="50" rows="4" value={JSON.stringify(value)} />
+
     </div>
   );
 }
