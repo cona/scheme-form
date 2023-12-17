90d72fefc5a59a5a60440191a099a72b94f462eb
s5g4 <uucona@gmail.com>
2023-12-17 18:15:40 +0900

    pages設定とoas追加

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-
 import './App.css';
 import { useCallback, useState } from 'react';
 
 import validator from '@rjsf/validator-ajv8';
 import { RJSFSchema } from '@rjsf/utils';
 import Form from '@rjsf/core';
-//import Form from '@rjsf/material-ui';
 
+//import openApiJson from "./oas/api.json";
+
+var { openapiSchemaToJsonSchema: toJsonSchema } = require("@openapi-contrib/openapi-schema-to-json-schema");
+
+/*
+const response = await fetch("./oas/api.json");
+const openApiJson = JSON.parse(await response.text());
+*/
 const schema: RJSFSchema = {
   title: 'Todo',
-  type: 'object',
-  required: ['title'],
-  properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "age": {
+      "type": "integer",
+      "minimum": 18
+    },
+    "email": {
+      "type": "string",
+      "format": "email"
+    },
+    "address": {
+      "type": "object",
+      "properties": {
+        "url": {
+          "type": "string"
+        },
+        "city": {
+          "type": "string"
+        },
+        "state": {
+          "type": "string",
+          "maxLength": 2
+        },
+        "zip": {
+          "type": "string",
+          "pattern": "\\d{5}"
+        }
+      },
+      "required": ["street", "city", "state", "zip"]
+    },
+    "phoneNumbers": {
+      "type": "array",
+      "items": {
+        "type": "string",
+        "pattern": "\\d{3}-\\d{3}-\\d{4}"
+      }
+    }
   },
+  "required": ["name","age", "email", "address", "phoneNumbers"]
 };
 
 const log = (type) => console.log.bind(console, type);
